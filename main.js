ea3ecdaeba662fdf22653c83801cac03931313ce
nataliao2613 <natalka.o2613@gmail.com>
2021-05-27 19:14:14 +0200

    dropdown structure changed and eventListener added

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let themeSwitcher = document.querySelector('.theme-switcher')
+let filtrationArrow = document.querySelector('.arrow')
+let optionsList = document.querySelector('.options')
+let filterBox = document.querySelector('.select')
+let show = false
 
 const switchTheme = () => {
     document.body.classList.toggle('dark-theme')
@@ -8,4 +12,22 @@ const switchTheme = () => {
     themeSwitcher.querySelector('i').classList.toggle('fas')
 }
 
+const showOptions = () => {
+    if(!show){
+        optionsList.style.display = 'block'  
+        show = !show
+    } 
+    else {
+        optionsList.style.display = 'none'
+        show = !show
+    } 
+}
+
+optionsList.onmouseleave = () => {
+    optionsList.style.display = 'none'
+        show = !show
+}
+
+
 themeSwitcher.addEventListener('click', switchTheme)
+filtrationArrow.addEventListener('click', showOptions)
