1c97d9f2437fcc41106506ce1919cf6b7aa3cefd
nataliao2613 <natalka.o2613@gmail.com>
2021-05-29 19:13:14 +0200

    comment deleted

--- country.js
+++ country.js
@@ -21,33 +21,6 @@ export default class Country{
     }
 }
 
-// export const loadContent = () => {
-//     let countries = []
-//     // let countriesList = document.querySelector('.container')
-//     axios.get('https://restcountries.eu/rest/v2/all')
-//     .then((response) => {
-//         response.data.forEach(c => {
-//             let country = new Country(c.name, c.flag, c.population, c.region, c.capital, c.nativeName, 
-//                 c.subregion, c.topLevelDomain, c.currencies, c.languages, c.borders)
-//                 countries.push(country)
-//             // let box = document.createElement('div')
-//             // box.classList.add('card')
-//             // box.innerHTML = createCountryCard(c)
-//             // countriesList.appendChild(box)
-//         })  
-//     })
-//     // .then(() => {
-//     //     let cards = document.querySelectorAll('.card')
-//     //     cards.forEach((c, id) => {
-//     //         c.addEventListener('click', () => {
-//     //             location.href = `/country-page.html`
-//     //             localStorage.setItem('countryId', id)
-//     //         })
-//     //     })
-//     // });
-//     return countries
-// }
-
 export const createCountryCard = (country) => {
     return (`
     <img src="${country.flag}" alt="flag_icon"/>
