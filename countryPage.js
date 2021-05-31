c2a2ddf9b48a17ce13e597365655e71d4398f0cc
nataliao2613 <natalka.o2613@gmail.com>
2021-05-31 19:17:20 +0200

    getting and displaying seleceted country borders

--- countryPage.js
+++ countryPage.js
@@ -14,6 +14,7 @@ let capital = document.querySelector('.capital')
 let domain = document.querySelector('.domain')
 let currencies = document.querySelector('.currencies')
 let languages = document.querySelector('.languages')
+let borders = document.querySelector('.details__borders')
 
 let countryCode = localStorage.getItem('countryCode')
 let country
@@ -21,7 +22,6 @@ let country
 axios.get(`https://restcountries.eu/rest/v2/alpha/${countryCode}`)
 .then((response) => {
     country = response.data
-    console.log(country)
 })
 .then(() => {
     flag.src = country.flag
@@ -41,9 +41,21 @@ axios.get(`https://restcountries.eu/rest/v2/alpha/${countryCode}`)
         id === 0 ? 
         languages.textContent += `${c.name}` : languages.textContent += `, ${c.name}`
     })
+    country.borders.map((c) => {
+        axios.get(`https://restcountries.eu/rest/v2/alpha/${c}`)
+        .then((response) => {
+            let borderCountry = response.data
+            let borderSpan = document.createElement('span')
+            borderSpan.textContent = borderCountry.name
+            borderSpan.addEventListener('click', () => {
+                localStorage.setItem('countryCode', c)
+                location.href = `/country-page.html`
+            })
+            borders.appendChild(borderSpan)
+        })
+
+    })
 })
-
-
 
 backButton.addEventListener('click', () => {
     location.href = '/index.html'
